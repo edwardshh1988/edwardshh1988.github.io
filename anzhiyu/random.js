f09a7f9eb90112e30b5f8e04b8b866d343053870
edwardshh1988 <131381936+edwardshh1988@users.noreply.github.com>
2023-05-07 13:23:19 +0000

    Delete source/_posts/TEST.md Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/07/TEST/","2023/05/07/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/07/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
