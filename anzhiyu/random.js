0241ea53cbb57b890af11f38da76e3ad7cf03844
edwardshh1988 <131381936+edwardshh1988@users.noreply.github.com>
2023-05-09 15:56:32 +0000

    Publish Post source/_posts/图片实验.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/09/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/09/hello-world/","2023/05/09/图片实验/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
