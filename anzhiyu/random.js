c7212205230efafc1d7ca8728a8ca2e99c3fd892
edwardshh1988 <131381936+edwardshh1988@users.noreply.github.com>
2023-05-21 12:59:19 +0000

    Publish Post source/_posts/20230521.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/21/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/21/20230521/","2023/05/21/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
